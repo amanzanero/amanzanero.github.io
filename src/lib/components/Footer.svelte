92f86ad1d8862534d2c063350c8bade1d3b02407
Andrew Manzanero <andrewmanzanero@proton.me>
2022-12-18 16:28:39 -0800

    publish

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import Spacer from './Spacer.svelte';
 </script>
 
-<footer class="bg-neutral py-5">
+<footer class="bg-neutral py-5 mt-5">
 	<Spacer>
 		<div class="footer text-neutral-content">
 			andrewmanzanero dot com, {new Date().getFullYear()}
